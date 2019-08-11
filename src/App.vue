8fcf7520a56117d7dd6774251540c394c3bc3a31
Stanley Huang <huang.stanley050@gmail.com>
2019-08-12 09:28:31 +1000

    header setup

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <template>
-  <div class="container"><router-view> </router-view></div>
+  <div class="container"><Header /><router-view> </router-view></div>
 </template>
 
 <script>
+import Header from "./components/Header.vue";
 export default {
   name: "app",
-  components: {}
+  components: { Header }
 };
 </script>
 
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,3 +1,45 @@
 <template>
-  <h1>header</h1>
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+        <router-link to="/" class="navbar-brand"
+          ><a>Stock Trader</a></router-link
+        >
+      </div>
+
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse">
+        <ul class="nav navbar-nav">
+          <router-link activeClass="active" tag="li" to="/portfolio">
+            <a>Portfolio</a>
+          </router-link>
+          <router-link activeClass="active" tag="li" to="/stocks">
+            <a>Stocks</a>
+          </router-link>
+        </ul>
+
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#">End Day</a></li>
+          <li class="dropdown">
+            <a
+              href="#"
+              class="dropdown-toggle"
+              data-toggle="dropdown"
+              role="button"
+              aria-haspopup="true"
+              aria-expanded="false"
+              >Save / Load <span class="caret"></span
+            ></a>
+            <ul class="dropdown-menu">
+              <li><a href="#">Save Data</a></li>
+              <li><a href="#">Load Data</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+      <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container-fluid -->
+  </nav>
 </template>
