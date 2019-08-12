48afa7d266f638527a040557768633da20f20a3f
Stanley Huang <huang.stanley050@gmail.com>
2019-08-13 09:38:46 +1000

    portfolio hooked up to vuex

--- src/components/portfolio/Portfolio.vue
+++ src/components/portfolio/Portfolio.vue
@@ -1,3 +1,20 @@
 <template>
-  <h1>Portfolio comp</h1>
+  <div>
+    <Stock v-for="stock in stocks" :stock="stock"> </Stock>
+  </div>
 </template>
+
+<script>
+import Stock from "./Stock.vue";
+import { mapGetters } from "vuex";
+export default {
+  computed: {
+    ...mapGetters({
+      stocks: "stockPortfolio"
+    })
+  },
+  components: {
+    Stock
+  }
+};
+</script>
