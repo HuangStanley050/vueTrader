1ea597d22b99d95cab96e41d04512056ff7e4b0a
Stanley Huang <huang.stanley050@gmail.com>
2019-08-12 10:00:03 +1000

    stocks component template done

--- src/App.vue
+++ src/App.vue
@@ -10,4 +10,8 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+body {
+  padding: 30px;
+}
+</style>
--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,3 +1,36 @@
 <template>
-  <h1>Stocks</h1>
+  <div><Stock v-for="stock in stocks"> </Stock></div>
 </template>
+
+<script>
+import Stock from "./Stock.vue";
+export default {
+  components: { Stock },
+  data() {
+    return {
+      stocks: [
+        {
+          id: 1,
+          name: "BMW",
+          price: 100
+        },
+        {
+          id: 2,
+          name: "Mercedes",
+          price: 200
+        },
+        {
+          id: 3,
+          name: "Honda",
+          price: 50
+        },
+        {
+          id: 4,
+          name: "Mazda",
+          price: 78
+        }
+      ]
+    };
+  }
+};
+</script>
