cb933cb354437d15bfa8cadae1e2bde48dc4ab10
Stanley Huang <huang.stanley050@gmail.com>
2019-08-14 10:35:13 +1000

    added route transition animatioin

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="container"><Header /><router-view> </router-view></div>
+  <div class="container">
+    <Header />
+    <transition name="slide" mode="out-in">
+      <router-view> </router-view>
+    </transition>
+  </div>
 </template>
 
 <script>
@@ -16,5 +21,31 @@ export default {
 <style>
 body {
   padding: 30px;
+}
+.slide-enter-active {
+  animation: slide-in 200ms ease-out forwards;
+}
+.slide-leave-active {
+  animation: slide-out 200ms ease-out forwards;
+}
+@keyframes slide-in {
+  from {
+    transform: translateY(-30px);
+    opacity: 0;
+  }
+  to {
+    transform: translateY(0);
+    opacity: 1;
+  }
+}
+@keyframes slid-out {
+  from {
+    transform: translateY(0);
+    opacity: 1;
+  }
+  to {
+    transform: translateY(-30px);
+    opacity: 0;
+  }
 }
 </style>
